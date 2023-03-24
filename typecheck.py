226c049548a6654123945f7c58250107c96ad035
windomin <dominik.winterer@inf.ethz.ch>
2023-03-24 15:21:35 +0100

    script to run typechecker on all files in a directory

--- typecheck.py
+++ typecheck.py
@@ -5,13 +5,20 @@ import sys
 from yinyang.src.parsing.Parse import parse_file
 from yinyang.src.parsing.Typechecker import typecheck
 
-if len(sys.argv) != 2:
-    print("Usage: typecheck.py <smt2-file>")
+if len(sys.argv) < 2:
+    print("Usage: typecheck.py <smt2-file> [--silent]")
     exit(2)
 
 def typecheck_smt2(fn):  
-    script, glob = parse_file(fn, silent=False)
+    script, glob = parse_file(fn, silent=True)
     typecheck(script, glob)
 
 if __name__ == "__main__":
-    typecheck_smt2(sys.argv[1])
+    if len(sys.argv) == 3 and sys.argv[2] == "--silent":
+        try:
+            typecheck_smt2(sys.argv[1])
+        except Exception as e: 
+            print(sys.argv[1])
+            exit(1)
+    else:
+         typecheck_smt2(sys.argv[1])
