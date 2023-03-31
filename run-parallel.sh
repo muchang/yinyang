e3d56ba023244151b7674d1a1cabc95acbeeb12a
Michael Moerschell <moemicha@student.ethz.ch>
2023-03-31 13:47:58 +0200

    Add --moderatelyverbose option to typecheck.py

--- run-parallel.sh
+++ run-parallel.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 directory="scripts/QF_BV"
-find $directory -name "*.smt2" -size -2k -print0 | parallel -0 -j8 ./typecheck.py {} --silent
+find $directory -name "*.smt2" -size -2k -print0 | parallel -0 -j8 ./typecheck.py {} --moderatelyverbose
 
--- typecheck.py
+++ typecheck.py
@@ -6,7 +6,7 @@ from yinyang.src.parsing.Parse import parse_file
 from yinyang.src.parsing.Typechecker import typecheck
 
 if len(sys.argv) < 2:
-    print("Usage: typecheck.py <smt2-file> [--silent]")
+    print("Usage: typecheck.py <smt2-file> [--silent | --moderatelyverbose]")
     exit(2)
 
 def typecheck_smt2(fn):  
@@ -22,6 +22,12 @@ if __name__ == "__main__":
             typecheck_smt2(sys.argv[1])
         except Exception as e: 
             print(sys.argv[1])
+            exit(1)
+    elif len(sys.argv) == 3 and sys.argv[2] == "--moderatelyverbose":
+        try:
+            typecheck_smt2(sys.argv[1])
+        except Exception as e:
+            print(f"[{sys.argv[1]}] {str(e)}")
             exit(1)
     else:
          typecheck_smt2(sys.argv[1])
