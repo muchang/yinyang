e88e220baef3ee76fb54828286cbb391b19144d9
Michael Moerschell <moemicha@student.ethz.ch>
2023-04-02 19:36:41 +0200

    Add progress bar back

--- run-parallel.sh
+++ run-parallel.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 directory="scripts/QF_BV"
-find $directory -name "*.smt2" -size -2k -print0 | parallel -0 -j8 --eta --progress ./typecheck.py {} --moderatelyverbose
+find $directory -name "*.smt2" -size -2k -print0 | parallel -0 -j8 --eta --progress --bar ./typecheck.py {} --moderatelyverbose
 
