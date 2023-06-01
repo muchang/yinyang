9f74f6b6f3bdea507d2933ccd33762c64b091004
Michael Moerschell <moemicha@student.ethz.ch>
2023-06-01 11:30:08 +0200

    Add Python script to precisely filter out benchmarks

---
+++ filter-benchmarks.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+import re
+import sys
+
+
+def keep(line: str) -> None:
+    try:
+        print(line.rstrip())
+    except BrokenPipeError:
+        pass
+
+
+def process_line(line: str, reject: str) -> None:
+    pattern = re.compile(r"scripts/(.*)\.smt2")
+    match = pattern.match(line)
+    if match is None:
+        # Something else
+        keep(line)
+        return
+    path = match.group(1).split("/")
+    if len(path) < 2:
+        # Not in a benchmark sub-folder
+        keep(line)
+        return
+    # Non-incremental
+    benchmark = path[0]
+    # Incremental
+    if benchmark == "incremental":
+        benchmark = path[1]
+    # Scan benchmark
+    for r in reject:
+        if r in benchmark:
+            return
+    keep(line)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: filter_benchmarks.py -v <benchmark>*")
+        exit(1)
+    reject = sys.argv[2:]
+    for line in sys.stdin.readlines():
+        process_line(line, reject)
