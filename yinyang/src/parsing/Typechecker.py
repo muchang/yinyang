de39ce1636b308fb3aaa344bcbcba38819b1631c
Michael Moerschell <moemicha@student.ethz.ch>
2023-06-06 18:10:32 +0200

    Rewrite typecheck_ite from scratch

--- yinyang/src/parsing/Typechecker.py
+++ yinyang/src/parsing/Typechecker.py
@@ -184,19 +184,23 @@ def typecheck_eq(expr, ctxt=[]):
     return BOOLEAN_TYPE
 
 
-def typecheck_ite(expr, ctxt=[]):
-    """(par (A) (ite Bool A A A))"""
-    typ = typecheck_expr(expr.subterms[0], ctxt)
-    if typecheck_expr(expr.subterms[0], ctxt) != BOOLEAN_TYPE:
-        t = typecheck_expr(expr.subterms[1], ctxt)
-        if not (is_subtype(t, typ) or is_subtype(typ, t)):
-            raise TypeCheckError(expr, expr.subterms[0], typ, BOOLEAN_TYPE)
-    t1 = typecheck_expr(expr.subterms[1], ctxt)
-    t2 = typecheck_expr(expr.subterms[2], ctxt)
-    if t1 != t2:
-        if not (is_subtype(t1, t2) or is_subtype(t2, t1)):
-            raise TypeCheckError(expr, expr.subterms[2], t1, t2)
-    return typecheck_expr(expr.subterms[1], ctxt)
+def typecheck_ite(expr, ctxt):
+    """(ite Bool A A A))"""
+    print(expr)
+    # Typecheck all subterms individually
+    assert len(expr.subterms) == 3, "ite takes 3 subterms"
+    ttypes = [typecheck_expr(x, ctxt) for x in expr.subterms]
+    # Bool
+    if ttypes[0] != BOOLEAN_TYPE:
+        raise TypeCheckError(expr, expr.subterms[0], BOOLEAN_TYPE, ttypes[0])
+    # A
+    if ttypes[1] is None:
+        raise TypeCheckError(expr, expr.subterms[1], "A", "None")
+    # A
+    if ttypes[2] != ttypes[1]:
+        raise TypeCheckError(expr, expr.subterms[2], ttypes[1], ttypes[2])
+    # A
+    return ttypes[1]
 
 
 def typecheck_nary_bool(expr, ctxt=[]):
