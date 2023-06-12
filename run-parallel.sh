faadc2d9104ebce4eeed9c20f9e96fffe2542909
Michael Moerschell <moemicha@student.ethz.ch>
2023-06-12 21:49:42 +0200

    Use advanced_pty.py over typecheck.py

--- run-parallel.sh
+++ run-parallel.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 directory="scripts/QF_AX"
-find $directory -iname "*.smt2" -print0 | parallel -0 -j8 --eta --progress --bar ./typecheck.py {} --info
+find $directory -iname "*.smt2" -print0 | parallel -0 -j8 --eta --progress --bar ./advanced_pty.py {} --info
